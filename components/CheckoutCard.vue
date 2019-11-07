5350e35d11f65a4e0ac2de76c7fd2951e5d1e230
Kendall Jackson <kendallkj00@gmail.com>
2019-11-07 17:37:49 -0500

    Initial Project Commit

---
+++ components/CheckoutCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'CheckoutCard'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/RugCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'RugCard'
+  }
+</script>
+
+<style scoped>
+
+</style>
