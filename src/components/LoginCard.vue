d5dc7e3d0960512cb8f0bdd448f75f6c5a1d6043
Kendall Jackson <kendallkj00@gmail.com>
2019-11-08 11:04:07 -0500

    Added patterned bg to logincard

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="loginroot pb-2 bg-gray-800 h-screen w-full">
+  <div class="loginroot pb-2 h-screen w-full">
     <div class="transition-all flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
       <div class="flex rounded-lg shadow-lg h-auto w-full sm:w-3/4 lg:w-1/2 bg-white sm:mx-0" style="min-height:500px; max-width: 700px;">
         <div class="flex flex-col w-full md:w-2/3 p-4">
@@ -9,36 +9,7 @@
             </h1>
             <div class="w-full mt-4 px-2">
               <div id="firebaseui-auth-container" />
-              <!-- <form class="form-horizontal w-3/4 mx-auto" method="POST" action="#">
-                <div class="flex flex-col mt-4">
-                  <input
-                    id="email"
-                    type="text"
-                    class="flex-grow h-8 px-2 border rounded border-grey-400"
-                    name="email"
-                    value=""
-                    placeholder="Email"
-                  >
-                </div>
-                <div class="flex flex-col mt-4">
-                  <input
-                    id="password"
-                    type="password"
-                    class="flex-grow h-8 px-2 rounded border border-grey-400"
-                    name="password"
-                    required
-                    placeholder="Password"
-                  >
-                </div>
-                <div class="flex items-center mt-4">
-                  <input id="remember" type="checkbox" name="remember" class="mr-2"> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
-                </div>
-                <div class="flex flex-col mt-8">
-                  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
-                    Login
-                  </button>
-                </div>
-              </form> -->
+              <!-- old form placement -->
               <div class="text-center mt-4">
                 <a class="no-underline hover:underline text-gray-600 text-xs" href="#">
                   Forgot Your Password?
@@ -74,6 +45,10 @@ export default {
 .loginpic {
   background: linear-gradient(to left, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.55)), url("https://firebasestorage.googleapis.com/v0/b/pooltablerugs.appspot.com/o/Images%2Fsamplerug.jpg?alt=media&token=44e0a8cd-2231-4058-baa9-a2eba8ef23d6")
 }
+.loginroot {
+    background-color: #38a168;
+    background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232a724b' fill-opacity='0.19'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  }
 @media (min-width: 1280px) {
   .loginroot {
     height: 92vh !important;
