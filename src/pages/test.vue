ab64523aa931058609c29d32521398df40e7a99b
Kendall Jackson <kendallkj00@gmail.com>
2019-11-18 18:05:39 -0500

    Added nuxt-stripe-module

---
+++ src/pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <div id="card-element" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted () {
+    const elements = this.$stripe.import().elements()
+    const card = elements.create('card')
+    // Add an instance of the card Element into the `card-element` <div>
+    card.mount('#card-element')
+  }
+}
+</script>
