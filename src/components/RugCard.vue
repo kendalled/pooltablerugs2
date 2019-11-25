cff813601679b56f4b8a0c1026eac682969f46eb
Kendall Jackson <kendallkj00@gmail.com>
2019-11-25 18:07:47 -0500

    Updated product image refs

--- src/components/RugCard.vue
+++ src/components/RugCard.vue
@@ -81,14 +81,14 @@
           >
             <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
           </svg>
-          <span class="ml-2 text-xs">{{ info.reviewCount }} reviews</span>
+          <span class="ml-2 text-xs text-gray-700">{{ info.reviewCount }} reviews</span>
         </div>
         <!-- commerce buttons -->
         <div class="flex pt-3 justify-between px-0">
           <button class="bg-white transition-bg hover:bg-gray-300 shadow text-gray-700 font-bold py-2 px-4 rounded" @click="seeDetails = !seeDetails">
             See Details
           </button>
-          <button class="bg-green-500 hover:bg-green-700 transition-bg shadow text-white font-bold py-2 px-4 rounded" @click="addToCart(info)">
+          <button class="bg-green-600 hover:bg-green-700 transition-bg shadow text-white font-bold py-2 px-4 rounded" @click="addToCart(info)">
             Add to Cart
           </button>
         </div>
