e7999c0bbed99b54e4ac172ca1f898bf42c1532c
Kendall Jackson <kendallkj00@gmail.com>
2019-11-27 15:49:48 -0500

    Add stripe tokenization

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -3,7 +3,7 @@
     <form id="payment-form" method="post" style="display: block;">
       <div class="form-row">
         <label for="card-element">
-          Credit or Debit:
+          Credit or Debit {{ uid }}:
         </label>
         <div id="card-element">
           <!-- A Stripe Element will be inserted here. -->
@@ -16,7 +16,7 @@
       <div class="col-12 col-12-xsmall">
         <br>
         <ul class="actions" style="float: right;">
-          <li><input type="submit" value="Continue"></li>
+          <li><input type="submit" class="text-center px-4 py-2 rounded shadow hover:bg-gray-200 cursor-pointer transition-bg focus:outline-none" value="Continue"></li>
         </ul>
         <br>
       </div>
@@ -27,9 +27,15 @@
 <script>
 import { firebase } from '~/plugins/firebase'
 export default {
+  data () {
+    return {
+      uid: null
+    }
+  },
   mounted () {
     // this.$store.commit('saveUserDetails')
-    const elements = this.$stripe.import().elements()
+    const stripe = this.$stripe.import()
+    const elements = stripe.elements()
     const card = elements.create('card')
     // Add an instance of the card Element into the `card-element` <div>
     card.mount('#card-element')
@@ -39,7 +45,7 @@ export default {
     form.addEventListener('submit', function (event) {
       event.preventDefault()
 
-      this.$stripe.createToken(card).then(function (result) {
+      stripe.createToken(card).then(function (result) {
         if (result.error) {
           // Inform the user if there was an error.
           const errorElement = document.getElementById('card-errors')
@@ -51,6 +57,18 @@ export default {
           })
         }
       })
+    })
+  },
+  created () {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.
+        this.uid = user.uid
+        console.log('signed in!')
+      } else {
+        // No user is signed in.
+        console.log('not signed in!')
+      }
     })
   }
 }
