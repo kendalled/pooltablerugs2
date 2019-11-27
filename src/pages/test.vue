076913bfa6c4ae440a8cf62c11a4d2b79018351f
Kendall Jackson <kendallkj00@gmail.com>
2019-11-27 14:51:14 -0500

    Testing ImageGroup.vue

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -23,12 +23,17 @@
     </form>
     <br>
     <br>
+    <ImageGroup />
   </div>
 </template>
 
 <script>
 import { firebase } from '~/plugins/firebase'
+import ImageGroup from '~/components/ImageGroup'
 export default {
+  components: {
+    ImageGroup
+  },
   mounted () {
     // this.$store.commit('saveUserDetails')
     const elements = this.$stripe.import().elements()
