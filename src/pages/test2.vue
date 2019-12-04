7183db46118f4fbacb8b0857b88c2073b355fe00
Kendall Jackson <kendallkj00@gmail.com>
2019-12-04 15:52:08 -0500

    Added new search

--- src/pages/test2.vue
+++ src/pages/test2.vue
@@ -2,16 +2,19 @@
   <div>
     <ImageGroup />
     <LoginForm />
+    <NewSearch />
   </div>
 </template>
 
 <script>
 import ImageGroup from '~/components/ImageGroup'
 import LoginForm from '~/components/LoginForm'
+import NewSearch from '~/components/NewSearch'
 export default {
   components: {
     ImageGroup,
-    LoginForm
+    LoginForm,
+    NewSearch
   }
 }
 </script>
