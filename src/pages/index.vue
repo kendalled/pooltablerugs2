b2f8694ee43834cb082e3a392c3528d134686848
Kendall Jackson <kendallkj00@gmail.com>
2019-12-05 12:42:01 -0500

    Added testimonial section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@
     <SplashHeader />
     <BlankSection />
     <FeaturedSection />
+    <TestimonialSection />
     <SubscribeEmail class="hidden lg:block" />
     <!-- <PricingSection /> -->
     <RespFooter />
@@ -12,6 +13,7 @@
 <script>
 import SplashHeader from '~/components/SplashHeader'
 import FeaturedSection from '~/components/FeaturedSection'
+import TestimonialSection from '~/components/TestimonialSection'
 import BlankSection from '~/components/BlankSection'
 import SubscribeEmail from '~/components/SubscribeEmail'
 import RespFooter from '~/components/RespFooter'
@@ -20,6 +22,7 @@ export default {
   components: {
     SplashHeader,
     FeaturedSection,
+    TestimonialSection,
     BlankSection,
     SubscribeEmail,
     RespFooter
