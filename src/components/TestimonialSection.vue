79471cda53aeca0ff5e2d96d39488387cebb99c3
Kendall Jackson <kendallkj00@gmail.com>
2019-12-05 13:22:30 -0500

    Update TestimonialSection.vue

--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -1,31 +1,31 @@
 <template>
   <section class="bg-gray-900 sm:py-32 py-16">
     <div class="container mx-auto sm:flex-row flex-col flex">
-      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-auto">
+      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-1.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-500 text-center my-3">
-          I just finished my trial period and was so amazed with the support and results that I purchased this template.
+          I received my rug and was amazed with the support and quality. My client's game room is complete.
         </p>
         <p class="font-bold  text-gray-400 text-center text-xl">
-          Sandy Grain - Designer
+          Sandy Grain - Interior Designer
         </p>
       </div>
-      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-auto">
+      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-2.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-500 text-center my-3">
-          Searching for a great marketing automation app was difficult but thankfully I found this template.
+          Searching for a sturdy, spill-proof rug was difficult. The huge selection from Pool Table Rugs was delightful.
         </p>
         <p class="font-bold  text-gray-400 text-center text-xl">
-          Bruce Cashman - Developer
+          Bruce Cash - Billiards Player
         </p>
       </div>
-      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-auto">
+      <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-3.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-500 text-center my-3">
-          Who would have thought that this template can provide such amazing results in just a few weeks of use.
+          My client wanted an outdoor shag rug. With hundreds of choices, it was easy to find the perfect one.
         </p>
         <p class="font-bold  text-gray-400 text-center text-xl">
-          Amanda Savage - Administrator
+          Amanda Savage - Interior Designer
         </p>
       </div>
     </div>
