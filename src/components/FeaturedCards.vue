363eb69ea6343c013f78b3fe6e42f366b0cd9522
Kendall Jackson <kendallkj00@gmail.com>
2019-12-05 17:39:35 -0500

    Fleshed out featured collection component

--- src/components/FeaturedCards.vue
+++ src/components/FeaturedCards.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="px-24 pb-4 flex flex-wrap items-center justify-center">
-    <div v-for="i in 3" :key="i" class="product flex flex-col items-start flex-shrink-0 m-4 bg-white max-w-xs w-full h-full rounded-lg shadow-lg px-6 bg-cover bg-center h-64 pt-4 pb-6" style="background-image:linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)), url('//a.storyblok.com/f/43708/1048x1048/ef79d343d8/tile-collection-bright.jpg');">
+  <div class="pb-4 flex flex-wrap items-center justify-center">
+    <div v-for="(style, i) in styles" :key="i" :style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.02), rgba(0, 0, 0, 0.28)), url(' + require('@/static/'+style.imageSrc) + ')' }" class="product flex flex-col items-start flex-shrink-0 m-4 bg-white max-w-xs w-full h-full rounded-lg shadow-lg px-6 bg-cover bg-center h-64 pt-4 pb-6">
       <!-- bg-green-600 -->
       <div class="flex w-full h-12 justify-end items-center ">
-        <div style="font-size: .7rem;" class="rounded-full bg-white text-gray-800 uppercase font-bold tracking-wide px-2 py-1">
+        <div v-show="style.new" style="font-size: .7rem;" class="rounded-full bg-white text-gray-800 uppercase font-bold tracking-wide px-2 py-1">
           NEW
         </div>
       </div>
-      <h3 class="flex items-end text-lg font-bold text-gray-100 text-left w-full mt-16 h-64">
-        Modern Rugs
+      <h3
+        class="cardlabel flex items-end text-lg font-bold text-gray-100 text-left w-full mt-16 h-64"
+      >
+        {{ style.title }} Rugs
       </h3>
     </div>
   </div>
@@ -16,19 +18,43 @@
 
 <style>
 .product {
-  transition-duration:.4s;
-  -webkit-transition-duration:.4s;
+  transition-duration:.3s;
+  -webkit-transition-duration:.3s;
 }
 .product:hover {
   transform:translateY(-6px);
   -ms-transform:translateY(-6px); /* prefix for IE 9 */
   -webkit-transform:translateY(-6px); /* prefix for Safari and Chrome */
 }
+.cardlabel {
+  text-shadow: 0px 4px 3px rgba(0,0,0,0.4), 0px 8px 13px rgba(0,0,0,0.1), 0px 18px 23px rgba(0,0,0,0.1);
+}
 
 </style>
 
 <script>
 export default {
-  name: 'FeaturedCards'
+  name: 'FeaturedCards',
+  data () {
+    return {
+      styles: [
+        {
+          title: 'Traditional',
+          new: false,
+          imageSrc: 'roomrug.jpg'
+        },
+        {
+          title: 'Modern',
+          new: true,
+          imageSrc: 'tinyrug.jpg'
+        },
+        {
+          title: 'Shag',
+          new: true,
+          imageSrc: 'whiteroom.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -2,29 +2,29 @@
   <section class="bg-gray-900 sm:py-32 py-16">
     <div class="container mx-auto sm:flex-row flex-col flex">
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
-        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-1.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
+        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-1.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-400 text-center my-3">
           I received my rug and was amazed with the support, speed, and quality. My client's game room is complete.
         </p>
-        <p class="font-semibold text-gray-400 text-center text-xl">
+        <p class="font-semibold text-gray-300 text-center text-xl">
           Sandy Grain - Interior Designer
         </p>
       </div>
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
-        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-2.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
+        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-2.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-400 text-center my-3">
           Searching for a sturdy, spill-proof rug was difficult. The huge selection from Pool Table Rugs was delightful.
         </p>
-        <p class="font-semibold text-gray-400 text-center text-xl">
+        <p class="font-semibold text-gray-300 text-center text-xl">
           Bruce Cash - Billiards Player
         </p>
       </div>
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 mx-4">
-        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-3.jpg" class="rounded-full w-24 h-24 mx-auto mb-2">
+        <img src="https://demos.onepagelove.com/html/leno/images/testimonial-3.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
         <p class="italic text-gray-400 text-center my-3">
           My client wanted an outdoor shag rug. With hundreds of choices, it was easy to find the perfect design.
         </p>
-        <p class="font-semibold text-gray-400 text-center text-xl">
+        <p class="font-semibold text-gray-300 text-center text-xl">
           Amanda Savage - Interior Designer
         </p>
       </div>
@@ -32,6 +32,16 @@
   </section>
 </template>
 
+<style scoped>
+.desat {
+    filter: none;
+    -webkit-filter: grayscale(0%);
+    -moz-filter: grayscale(0%);
+    -ms-filter: grayscale(0%);
+    -o-filter: grayscale(0%);
+    cursor: pointer;
+}
+</style>
 <script>
 export default {
   name: 'TestimonialSection'
