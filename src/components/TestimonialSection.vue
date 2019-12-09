809c7d4b96230402b40d8376fc89d1ba98d92b3c
Kendall Jackson <kendallkj00@gmail.com>
2019-12-09 13:06:43 -0500

    px-2 on small devices

--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto sm:flex-row flex-col flex">
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 md:mx-2 lg:mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-1.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
-        <p class="italic text-gray-400 text-center my-3">
+        <p class="italic text-gray-400 text-center my-3 px-2 lg:px-0">
           I received my rug and was amazed with the support, speed, and quality. My client's game room is complete.
         </p>
         <p class="font-semibold text-gray-300 text-center text-xl">
@@ -12,7 +12,7 @@
       </div>
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 md:mx-2 lg:mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-2.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
-        <p class="italic text-gray-400 text-center my-3">
+        <p class="italic text-gray-400 text-center my-3 px-2 lg:px-0">
           Searching for a sturdy, spill-proof rug was difficult. The huge selection from Pool Table Rugs was delightful.
         </p>
         <p class="font-semibold text-gray-300 text-center text-xl">
@@ -21,7 +21,7 @@
       </div>
       <div class="sm:w-1/3 w-full sm:mb-0 mb-16 md:mx-2 lg:mx-4">
         <img src="https://demos.onepagelove.com/html/leno/images/testimonial-3.jpg" class="desat rounded-full w-24 h-24 mx-auto mb-2">
-        <p class="italic text-gray-400 text-center my-3">
+        <p class="italic text-gray-400 text-center my-3 px-2 lg:px-0">
           My client wanted an outdoor shag rug. With hundreds of choices, it was easy to find the perfect design.
         </p>
         <p class="font-semibold text-gray-300 text-center text-xl">
