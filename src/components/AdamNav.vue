00a74acd19661b6ad4540bffb2dcb0e24f215ec9
Kendall Jackson <kendallkj00@gmail.com>
2019-12-11 11:41:32 -0500

    Added sign-in link

--- src/components/AdamNav.vue
+++ src/components/AdamNav.vue
@@ -47,6 +47,12 @@
                 <span class="text-sm font-semibold">{{ cartCount }}</span>
               </div>
             </nuxt-link>
+            <nuxt-link
+              to="login"
+              class="ml-8 px-3 py-2 font-medium text-center text-sm rounded-lg bg-gray-300 text-gray-900 hover:bg-gray-400 focus:outline-none transition-bg"
+            >
+              Sign In
+            </nuxt-link>
           </div>
         </div>
       </nav>
