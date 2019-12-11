54f91bdd0fc811cd7c5095449b6691a618f11979
Kendall Jackson <kendallkj00@gmail.com>
2019-12-11 14:00:09 -0500

    Sequential entrance faster

--- src/pages/rugs.vue
+++ src/pages/rugs.vue
@@ -19,19 +19,19 @@
           </div>
           <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
             <div class="px-4 sm:inline-flex items-center sm:pt-2 sm:pb-8 xl:px-8">
-              <sequential-entrance>
+              <sequential-entrance delay="150">
                 <div v-for="item in traditional" v-show="i === 0" :key="item.sku" :class="{'mt-10 sm:ml-4': i >= 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0 md:inline-block xl:mx-6">
                   <RugCard :info="item" />
                 </div>
               </sequential-entrance>
 
-              <sequential-entrance>
+              <sequential-entrance delay="150">
                 <div v-for="item in shag" v-show="i === 1" :key="item.sku" :class="{'mt-10 sm:ml-4': i >= 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0 md:inline-block xl:mx-6">
                   <RugCard :info="item" />
                 </div>
               </sequential-entrance>
 
-              <sequential-entrance>
+              <sequential-entrance delay="150">
                 <div v-for="item in modern" v-show="i === 2" :key="item.sku" :class="{'mt-10 sm:ml-4': i >= 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0 md:inline-block xl:mx-6">
                   <RugCard :info="item" />
                 </div>
