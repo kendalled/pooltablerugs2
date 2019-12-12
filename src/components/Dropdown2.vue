d5365b360a8a25661dfc62e50bdb02a9045e303f
Kendall Jackson <kendallkj00@gmail.com>
2019-12-12 17:47:29 -0500

    Dropdown2, Hero2

---
+++ src/components/Dropdown2.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="dropdown" class="relative inline-block">
+    <button
+      @click="opened = true"
+      class="inline-flex items-center pl-6 pr-2 py-2 font-semibold text-white bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:shadow-outline active:bg-gray-900"
+    >
+      Account
+      <svg
+        class="ml-2 h-6 w-6 text-white"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
+        />
+      </svg>
+    </button>
+
+    <div
+      v-if="opened"
+      @click="opened = false"
+      class="fixed inset-0"
+      tabindex="-1"
+    />
+
+    <transition
+      enter-class="opacity-0 scale-90"
+      enter-active-class="ease-out transition-fastest"
+      enter-to-class="opacity-100 scale-100"
+      leave-class="opacity-100 scale-100"
+      leave-active-class="ease-in transition-fastest"
+      leave-to-class="opacity-0 scale-90"
+    >
+      <div
+        v-if="opened"
+        class="mt-2 absolute right-0 origin-top-right text-left"
+      >
+        <div class="w-64 bg-white rounded-lg shadow-lg">
+          <div class="flex items-center px-6 py-4">
+            <img
+              class="h-10 w-10 rounded-full flex-no-shrink"
+              src="https://images.unsplash.com/photo-1541271696563-3be2f555fc4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=1.75&w=200&h=200&q=80"
+              alt=""
+            >
+            <div class="ml-4">
+              <p class="font-semibold text-gray-900 leading-none">
+                Margot Foster
+              </p>
+              <p>
+                <a
+                  href="#"
+                  class="text-sm text-gray-600 leading-none hover:underline"
+                >View Profile</a>
+              </p>
+            </div>
+          </div>
+          <div class="border-t-2 border-gray-200 py-1">
+            <a
+              href="#"
+              class="block px-6 py-3 leading-tight hover:bg-gray-200"
+            >Settings and Privacy</a>
+            <a
+              href="#"
+              class="block px-6 py-3 leading-tight hover:bg-gray-200"
+            >Language</a>
+          </div>
+          <div class="border-t-2 border-gray-200 py-1">
+            <a
+              href="#"
+              class="block px-6 py-3 leading-tight hover:bg-gray-200"
+            >Advertise</a>
+            <a
+              href="#"
+              class="block px-6 py-3 leading-tight hover:bg-gray-200"
+            >Analytics</a>
+          </div>
+          <form
+            class="border-t-2 border-gray-200 py-1"
+            action="#"
+            method="POST"
+          >
+            <button
+              class="block w-full px-6 py-3 text-left leading-tight hover:bg-gray-200"
+            >
+              Sign out
+            </button>
+          </form>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown2',
+  data () {
+    return {
+      opened: true
+    }
+  }
+}
+</script>
---
+++ src/components/Hero2.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="antialiased">
+    <div class="px-8 pt-2 lg:pt-8 pb-16 max-w-xl mx-auto lg:max-w-full lg:px-0 lg:pt-24 lg:pb-32 lg:relative lg:max-h-screen bg-white">
+      <div class="xl:max-w-6xl lg:mr-0 lg:ml-32">
+        <div class="lg:w-7/12 lg:pl-32 lg:pr-8">
+          <div class="mt-8 lg:mt-0 lg:absolute lg:inset-y-0 lg:right-0 lg:w-5/12">
+            <picture>
+              <source srcset="~/static/original1920.webp" type="image/webp">
+              <source srcset="~/static/original1920.jpg" type="image/jpeg">
+              <img class="h-56 w-full object-cover object-center sm:h-56 rounded-lg shadow-xl lg:inset-0 lg:h-full lg:object-right lg:shadow-none lg:rounded-none" src="~/static/original1920.jpg" alt="Game room with a pool table rug.">
+            </picture>
+            <!-- <svg
+              style="margin-left: -0.015rem;"
+              class="hidden lg:block absolute inset-y-0 left-0 w-20 h-full"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <polygon class="text-white" fill="currentColor" points="0,0 0,100 100,0" />
+            </svg> -->
+          </div>
+          <h1 class="mt-4 lg:mt-8 text-3xl sm:text-4xl font-bold text-gray-900 leading-tight">
+            Protect your game room
+            <br class="hidden sm:inline">
+            and <span class="text-green-700">pool table</span> with a <span class="italic font-bold text-blue-700">rug</span>.
+          </h1>
+          <p class="mt-4 text-gray-700 tracking-tight text-xl">
+            <span class="hidden lg:block">Protect your game room with our custom rugs.</span>From spills to scuffs, help tidy up your game room.
+          </p>
+          <div class="mt-6">
+            <button aria-label="About Our Rugs" class="bg-white transition-bg hover:bg-gray-100 text-gray-800 font-semibold py-3 px-6 border border-gray-400 rounded shadow">
+              Learn More
+            </button>
+            <nuxt-link to="/rugs">
+              <button aria-label="Shop Rugs" class="px-6 py-3 inline-block ml-4 bg-green-700 hover:bg-green-700 rounded text-white font-semibold transition-bg tracking-wide shadow-md focus:outline-none">
+                Shop Rugs
+              </button>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero2'
+}
+</script>
+
+<style scoped>
+
+</style>
