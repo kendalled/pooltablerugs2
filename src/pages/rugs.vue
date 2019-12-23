0480d6e82d7eb6ae73b4dce784491d8d757b96a6
Kendall Jackson <kendallkj00@gmail.com>
2019-12-23 18:03:05 -0500

    Infinite scroll implemented

--- src/pages/rugs.vue
+++ src/pages/rugs.vue
@@ -32,6 +32,28 @@
             </div>
           </div>
         </transition>
+        <transition v-for="(row, i) in doc2" v-if="true" :key="i" name="fade">
+          <div :class="{'mt-6': i > 0}" class="w-full">
+            <div class="px-4 xl:px-8">
+              <h3 class="antialiased text-gray-800 font-semibold text-xl text-center">
+                Your Personalized Rugs
+              </h3>
+              <p class="text-gray-700 text-center">
+                Infinite Scroll, Baby
+              </p>
+            </div>
+            <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
+              <div class="px-4 sm:inline-flex items-center sm:pt-2 sm:pb-8 xl:px-8">
+                <div v-for="(item, j) in doc2[i]" :key="j" :class="{'mt-10 sm:ml-4': i >= 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0 md:inline-block xl:mx-6">
+                  <RugCard :info="item" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </transition>
+        <client-only>
+          <infinite-loading @infinite="infiniteHandler" />
+        </client-only>
       </main>
     </div>
   </div>
@@ -40,7 +62,7 @@
 <script>
 import RugCard from '~/components/RugCard'
 // import { firebase, fireDb }
-// import { firebase } from '~/plugins/firebase'
+import { fireDb } from '~/plugins/firebase'
 import SearchFilters from '~/components/SearchFilters'
 
 export default {
@@ -56,6 +78,7 @@ export default {
   },
   data () {
     return {
+      scrollPosY: 0,
       shown: true,
       traditional: [ { 'sku': 'A108-23', 'foldURL': '/traditional/fold/a108-23-fold.jpg', 'frontURL': '/traditional/room/roomrug.jpg', 'MSRP': 255, 'styling': 'Traditional', 'romance': 'The Ancient Treasures Collection showcases traditional inspired designs that exemplify timeless styles of elegance, comfort, and sophistication. With their hand tufted construction, these rugs offer an affordable alternative to other handmade constructions while perserving the same natural demeanor and charm. Made with NZ Wool in India, and has Medium Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Ancient Treasures', 'rating': 5, 'reviewCount': 12, 'group': "2' x 3'" }, { 'sku': 'A108-268', 'foldURL': '/traditional/fold/a108-268-fold.jpg', 'frontURL': '/traditional/room/roomrug.jpg', 'MSRP': 900, 'styling': 'Traditional', 'romance': 'The Ancient Treasures Collection showcases traditional inspired designs that exemplify timeless styles of elegance, comfort, and sophistication. With their hand tufted construction, these rugs offer an affordable alternative to other handmade constructions while perserving the same natural demeanor and charm. Made with NZ Wool in India, and has Medium Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Ancient Treasures', 'rating': 5, 'reviewCount': 12, 'group': "2'6\" x 8'" }, { 'sku': 'A108-3353', 'foldURL': '/traditional/fold/a108-3353-fold.jpg', 'frontURL': '/traditional/room/roomrug.jpg', 'MSRP': 765, 'styling': 'Traditional', 'romance': 'The Ancient Treasures Collection showcases traditional inspired designs that exemplify timeless styles of elegance, comfort, and sophistication. With their hand tufted construction, these rugs offer an affordable alternative to other handmade constructions while perserving the same natural demeanor and charm. Made with NZ Wool in India, and has Medium Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Ancient Treasures', 'rating': 5, 'reviewCount': 12, 'group': "3' x 5' - 4' x 6'" }, { 'sku': 'A108-58', 'foldURL': '/traditional/fold/a108-58-fold.jpg', 'frontURL': '/traditional/room/roomrug.jpg', 'MSRP': 1555, 'styling': 'Traditional', 'romance': 'The Ancient Treasures Collection showcases traditional inspired designs that exemplify timeless styles of elegance, comfort, and sophistication. With their hand tufted construction, these rugs offer an affordable alternative to other handmade constructions while perserving the same natural demeanor and charm. Made with NZ Wool in India, and has Medium Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Ancient Treasures', 'rating': 5, 'reviewCount': 12, 'group': "5' x 8'" } ],
       shag: [ { 'sku': 'AAS2300-23', 'foldURL': '/shag/fold/aas2300-23-fold.jpg', 'frontURL': '/shag/room/roomrug.jpg', 'MSRP': 85, 'styling': 'Shag', 'romance': 'The shag pieces from our Alaska Shag Collection emanate comfortability, merging both vintage and contemporary thought to create timeless works that both your eyes and feet can appreciate! The meticulously woven construction of these pieces boasts durability and will provide natural charm into your decor space. Made with Polyester in Turkey, and has Plush Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Alaska Shag', 'rating': 4, 'reviewCount': 11, 'group': "2' x 3'" }, { 'sku': 'AAS2300-2773', 'foldURL': '/shag/fold/aas2300-2773-fold.jpg', 'frontURL': '/shag/room/roomrug.jpg', 'MSRP': 220, 'styling': 'Shag', 'romance': 'The shag pieces from our Alaska Shag Collection emanate comfortability, merging both vintage and contemporary thought to create timeless works that both your eyes and feet can appreciate! The meticulously woven construction of these pieces boasts durability and will provide natural charm into your decor space. Made with Polyester in Turkey, and has Plush Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Alaska Shag', 'rating': 4, 'reviewCount': 11, 'group': "2'6\" x 8'" }, { 'sku': 'AAS2300-5373', 'foldURL': '/shag/fold/aas2300-5373-fold.jpg', 'frontURL': '/shag/room/roomrug.jpg', 'MSRP': 375, 'styling': 'Shag', 'romance': 'The shag pieces from our Alaska Shag Collection emanate comfortability, merging both vintage and contemporary thought to create timeless works that both your eyes and feet can appreciate! The meticulously woven construction of these pieces boasts durability and will provide natural charm into your decor space. Made with Polyester in Turkey, and has Plush Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Alaska Shag', 'rating': 4, 'reviewCount': 11, 'group': "5' x 8'" }, { 'sku': 'AAS2300-710103', 'foldURL': '/shag/fold/aas2300-710103-fold.jpg', 'frontURL': '/shag/room/roomrug.jpg', 'MSRP': 780, 'styling': 'Shag', 'romance': 'The shag pieces from our Alaska Shag Collection emanate comfortability, merging both vintage and contemporary thought to create timeless works that both your eyes and feet can appreciate! The meticulously woven construction of these pieces boasts durability and will provide natural charm into your decor space. Made with Polyester in Turkey, and has Plush Pile. Spot Clean Only, One Year Limited Warranty.', 'collection': 'Alaska Shag', 'rating': 4, 'reviewCount': 11, 'group': "8' x 10'" } ],
@@ -63,6 +86,8 @@ export default {
       // doc: [],
       // doc2: [],
       // doc3: [],
+      doc: [],
+      doc2: [],
       styles: [
         {
           title: 'Traditional',
@@ -79,11 +104,17 @@ export default {
           description: 'Vivid colors, bold designs, and sharp lines. Our modern rugs will match any 21st century space.',
           visible: true
         }
+      ],
+      newRugs: [
+        {
+          title: 'sample',
+          visible: true
+        }
       ]
     }
   },
   // async asyncData ({ app, params, error }) {
-  //   const rugRef = fireDb.collection('Rugs').where('Style', '==', 'Traditional').limit(4)
+  //   const rugRef = fireDb.collection('Rugs').where('Style', '==', 'Traditional').where('Size Group', '==', '5\' x 8\'').limit(4)
   //   const rugRef2 = fireDb.collection('Rugs').where('Style', '==', 'Shag').limit(4)
   //   const rugRef3 = fireDb.collection('Rugs').where('Style', '==', 'Modern').limit(4)
   //   let data, data2, data3
@@ -133,6 +164,44 @@ export default {
   //   }
   // },
   methods: {
+    infiniteHandler ($state) {
+      setTimeout(() => {
+      // const rugRef = await fireDb.collection('Rugs').where('Style', '==', 'Traditional').where('Size Group', '==', '5\' x 8\'').limit(4)
+        let rugRef = fireDb.collection('Rugs').where('Style', '==', 'Traditional').limit(8)
+        const vm = this
+        rugRef.get().then(function (querySnapshot) {
+          let results
+          results = null
+          results = []
+          let collection
+          collection = null
+          collection = []
+          querySnapshot.forEach(function (doc) {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, ' => ', doc.data())
+            collection.push({ sku: doc.data().SKU, foldURL: 'https://firebasestorage.googleapis.com/v0/b/pooltablerugs.appspot.com/o/FoldTest%2Fthumbs%2Fa108-268-fold_300x300.jpg?alt=media&token=0e56ad3c-39e3-442c-9353-a3ad60799fcd' + doc.data().SKU.toLowerCase() + '-fold_300x300.jpg', frontURL: 'https://storage.googleapis.com/pooltablerugs.appspot.com/skus/ath5111-24hm.jpg', MSRP: doc.data().MSRP, styling: doc.data().Style, romance: doc.data()['Romance Copy'], collection: doc.data().Collection, rating: 4, reviewCount: 11, group: doc.data()['Size Group'] })
+          })
+          // while (collection) {
+          //   results.push(collection.splice(0, 4))
+          // }
+          for (let i = 0; i < collection.length; i++) {
+            results.push(collection.splice(0, 4))
+          }
+          for (let i = 0; i < results.length; i++) {
+            console.log('working')
+            vm.doc2.push(results[i])
+          }
+          results = null
+        })
+        rugRef = null
+        setTimeout(() => {
+          $state.complete()
+        }, 500)
+      }, 1000)
+      setTimeout(() => {
+        $state.loaded()
+      }, 250)
+    },
     truncateString (str, num) {
       // If the length of str is less than or equal to num
       // just return str--don't truncate it.
