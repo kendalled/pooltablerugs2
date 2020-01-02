96945581c86a2c92192e384ec2831391ebd19978
Kendall Jackson <kendallkj00@gmail.com>
2020-01-02 17:17:53 -0500

    decreased pattern opacity

--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -143,6 +143,6 @@ export default {
 <style scoped>
 .styled {
   background-color: #fbfbfb;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23111111' fill-opacity='0.1' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23111111' fill-opacity='0.075' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 }
 </style>
