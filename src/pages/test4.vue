e3fa47225bbea4a7badc58a57055caef5b0c7f2d
Kendall Jackson <kendallkj00@gmail.com>
2020-01-13 10:59:22 -0500

    Testing css only carousel on touch

---
+++ src/pages/test4.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex w-full h-screen bg-blue-600 justify-center items-center">
+    <CarouselDemo />
+  </div>
+</template>
+
+<script>
+import CarouselDemo from '~/components/CarouselDemo'
+export default {
+  components: {
+    CarouselDemo
+  }
+}
+</script>
