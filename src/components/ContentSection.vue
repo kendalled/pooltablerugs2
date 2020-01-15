bfd1c4187bf1895d5089baa5edd725532929632a
Kendall Jackson <kendallkj00@gmail.com>
2020-01-15 13:16:52 -0500

    Changed to dark theme

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,59 +1,67 @@
 <template>
   <div class="px-16 pt-4 pb-12 bg-gray-800">
-    <h1 class="text-3xl mt-6 text-green-400 text-center px-10">
-      Equal height boxes using Tailwind CSS
+    <h1 class="text-3xl mt-12 text-green-400 font-bold text-center px-10">
+      Get started with the facts.
     </h1>
 
     <h2 class="text-lg mb-8 mt-2 px-10 text-center text-gray-100">
-      An equal height grid with content that grows, aligning links at bottom
+      From rug use-cases to support info, read about our services below.
     </h2>
 
     <ul class="flex flex-wrap">
       <li class="w-full sm:w-1/2 flex flex-col">
-        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-2 py-6 px-8">
-          <h2 class="text-2xl mb-2">
-            Pool table rugs are a necessity for any game room.
+        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-6 py-6 px-8">
+          <h2 class="text-2xl mb-4 text-gray-900 font-semibold">
+            Rugs are a necessity for any game room.
           </h2>
-          <p class="flex-grow border">
-            Most importantly, they’ll protect your floor against scratches and scuff marks. They serve to highlight your pool table, softening stone or wood floors, as well as balancing color and pattern in a room.
+          <p class="flex-grow text-gray-800">
+            Most importantly, they’ll protect your floor against scratches and scuff marks. They serve to highlight your pool table, softening stone or wood floors, as well as balancing color in a room.
           </p>
-          <a href="" class="my-4 underline">Link</a>
+          <button class="bg-transparent hover:bg-blue-700 text-blue-600 underline font-bold py-2 px-4 rounded w-full my-6">
+            Learn More
+          </button>
         </div>
       </li>
 
       <li class="w-full sm:w-1/2 flex flex-col">
-        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-2 py-6 px-8">
-          <h2 class="text-2xl mb-2">
+        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-6 py-6 px-8">
+          <h2 class="text-2xl mb-4 text-gray-900 font-semibold">
             Rugs help reduce noise drastically.
           </h2>
-          <p class="flex-grow">
+          <p class="flex-grow text-gray-800">
             Soft materials such as rugs can absorb the sound of clacking pool balls. Footsteps around the game tables are reduced drastically as well. Don't underestimate the impact!
           </p>
-          <a href="" class="my-4 underline">Link</a>
+          <button class="bg-transparent hover:bg-blue-700 text-blue-600 underline font-bold py-2 px-4 rounded w-full my-6">
+            Learn More
+          </button>
         </div>
       </li>
 
       <li class="w-full sm:w-1/2 flex flex-col">
-        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-2 py-6 px-8">
-          <h2 class="text-2xl mb-2">
+        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-6 py-6 px-8">
+          <h2 class="text-2xl mb-4 text-gray-900 font-semibold">
             Our Goal
           </h2>
-          <p class="flex-grow">
-            Our mission is to provide our customers with a high-quality, durable, and stylish rug that they are guaranteed to love. From traditional to more modern and psychedelic designs, we have curated a selection of rugs that will make the perfect addition to any game room. We are here to help you choose a rug that will not only look good, but will keep your game room safe from spills, scratches, and any other damage.
+          <p class="flex-grow text-gray-800">
+            Our mission is to provide our customers with a high-quality, durable, and stylish rug that they are guaranteed to love. From traditional to more modern and psychedelic designs, we have curated a selection of rugs that will make the perfect addition to any game room. We are here to help you choose a rug that will not only look good, but will keep your game room safe.
           </p>
-          <a href="" class="my-4 underline">Link</a>
+          <button class="bg-transparent hover:bg-blue-700 text-blue-600 underline font-bold py-2 px-4 rounded w-full my-6">
+            Learn More
+          </button>
         </div>
       </li>
 
       <li class="w-full sm:w-1/2 flex flex-col">
-        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-2 py-6 px-8">
-          <h2 class="text-2xl mb-2">
-            24/7 Support
+        <div class="flex flex-col h-full bg-gray-100 rounded shadow m-6 py-6 px-8">
+          <h2 class="text-2xl mb-4 text-gray-900 font-semibold">
+            Live Support
           </h2>
-          <p class="flex-grow">
-            Our 24/7 support is available anytime you need help with the ordering process. Ask any time for design assistance, sizing recommendations, and order tracking. Get started with a livechat message, e-mail, or phone call. We have designers in-house to help you find the perfect rug.
+          <p class="flex-grow text-gray-800">
+            Our 24/7 support is available anytime you need help with the ordering process. Ask any time for design assistance, sizing recommendations, and order tracking. Get started with a livechat message, e-mail, or phone call. We have designers in-house to help you find the perfect rug. We are here to help you choose a rug that will not only look good, but will keep your game room safe from spills, scratches, and any other damage.
           </p>
-          <a href="" class="my-4 underline">Link</a>
+          <button class="bg-transparent hover:bg-blue-700 text-blue-600 underline font-bold py-2 px-4 rounded w-full my-6">
+            Learn More
+          </button>
         </div>
       </li>
     </ul>
